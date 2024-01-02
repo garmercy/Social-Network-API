12ce0c045d23c643dae5e9d0bdcc23c771f1e822
Cristina Garcia <garmercy.mar@gmail.com>
2024-01-02 20:53:15 +1100

    routes edited

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,15 +7,25 @@ const {
   deleteUser,
   addFriend,
   removeFriend,
-} = require("../../controllers/user-controller");
+} = require("../../controllers/userController");
 
 // /api/users
-router.route("/").get(getAllUser).post(createUser);
+router
+  .route("/")
+  .get(getAllUser)
+  .post(createUser);
 
 // /api/users/:id
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router
+  .route("/:id")
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
-router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+router
+  .route("/:userId/friends/:friendId")
+  .post(addFriend)
+  .delete(removeFriend);
 
 module.exports = router;
