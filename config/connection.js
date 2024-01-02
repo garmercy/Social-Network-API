1ef305c2680fa974c46e4405050ed17f10923129
Cristina Garcia <garmercy.mar@gmail.com>
2024-01-02 18:51:30 +1100

    adding changes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
 //connect with mongodb file
-connect('mongodb://127.0.0.1:27017/videosAndResponses');
+connect('mongodb://127.0.0.1:27017/socialNet');
 
 module.exports = connection;
